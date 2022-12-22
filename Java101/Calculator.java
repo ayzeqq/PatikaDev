65c7491760e33f6776920612809f7fa676b9a77a
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2022-12-23 01:18:26 +0300

    Create Calculator.java

---
+++ Java101/Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner (System.in);
+        System.out.println("İlk sayıyı giriniz:");
+        int a=input.nextInt();
+        System.out.println("İkinci sayıyı giriniz:");
+        int b=input.nextInt();
+        System.out.println("Yapmak istediğiniz işlemi seçiniz: +,-,*,/");
+        String c = input.next();
+        switch (c) {
+            case "+":
+                System.out.println(a+b);
+                break;
+            case "-":
+                System.out.println(a-b);
+                break;
+            case "*":
+                System.out.println(a*b);
+                break;
+            case "/":
+                if(b!=0){
+                    System.out.println(a/b);
+                }
+                else{
+                    System.out.println("Sayı sıfıra bölünemez!");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
