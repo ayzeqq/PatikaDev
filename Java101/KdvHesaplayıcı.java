402a094ec3715b4715d8417d8755ddddbada35d4
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2022-12-26 19:19:44 +0300

    Update KdvHesaplayıcı.java

--- Java101/KdvHesaplayıcı.java
+++ Java101/KdvHesaplayıcı.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class KDV {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Fiyatı giriniz:");
         double fiyat = input.nextDouble();
         if(fiyat>0 && fiyat<1000){
             System.out.println("KDV'siz Fİyat: " + fiyat);
             System.out.println("KDV'li Fİyat: " + fiyat*1.18);
-            System.out.println("KDV tutarı: " + fiyat*0.18);
+            System.out.println("KDV tutarı: " + fiyat*0.18 + " " + "(%18)");
         }
         else if(fiyat<0){
             System.out.println("Değer 0'dan küçük girilemez!");
@@ -15,7 +16,7 @@ public class KDV {
         else{
             System.out.println("KDV'siz Fİyat: " + fiyat);
             System.out.println("KDV'li Fİyat: " + fiyat*1.08);
-            System.out.println("KDV tutarı: " + fiyat*0.08);
+            System.out.println("KDV tutarı: " + fiyat*0.08 + " " + "(%8)");
         }
     }
 }
