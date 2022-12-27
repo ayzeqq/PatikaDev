0db74c0ac87b431e70dbc6805c1174d96a62a4a1
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2022-12-27 18:23:08 +0300

    Update Taksimetre.java

--- Java101/Taksimetre.java
+++ Java101/Taksimetre.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Taksimetre {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
-        System.out.print("Gidilen mesafeyi giriniz: ");
+        System.out.print("Gidilen mesafeyi giriniz(km): ");
         int km=input.nextInt();
         double tutar=10+km*2.2;
         if(tutar<20){
             tutar=20;
         }
-        System.out.println(tutar);
+        System.out.println("Tutar: " + tutar + " TL");
 
     }
 }
