91ee4da38774b1d16f558c1ded0afc8ba061af81
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2023-01-02 11:39:29 +0300

    Update KüçüktenBüyüğe.java

--- Java101/KüçüktenBüyüğe.java
+++ Java101/KüçüktenBüyüğe.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class KüçüktenBüyüğe {
     public static void main(String[] args) {
     Scanner input=new Scanner(System.in);
-    System.out.print("İlk sayıyı giriniz: ");
+    System.out.print("İlk sayıyı(a) giriniz: ");
     int a=input.nextInt();
-    System.out.print("İkinci sayıyı giriniz: ");
+    System.out.print("İkinci sayıyı(b) giriniz: ");
     int b=input.nextInt();
-    System.out.print("Üçüncü sayıyı giriniz: ");
+    System.out.print("Üçüncü sayıyı(c) giriniz: ");
     int c=input.nextInt();
     if(a<b && a<c){
         if(b<c){
