698d49dcdb7d0871e7c6ab2783df0be4d7c893be
ayzeqq <99403758+ayzeqq@users.noreply.github.com>
2023-02-04 16:45:57 +0300

    Add files via upload

---
+++ Java101/OgrenciBilgiSistemi/Course.java
@@ -0,0 +1,36 @@
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    int sozluNote;
+
+    public Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+    }
+
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("İşlem başarılı");
+        } else {
+            System.out.println(t.name + " Akademisyeni bu dersi veremez.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " dersinin Akademisyeni : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + " dersine Akademisyen atanmamıştır.");
+        }
+    }
+
+    public double calcNote () {
+        return note*0.8+sozluNote*0.2;
+    }
+}
